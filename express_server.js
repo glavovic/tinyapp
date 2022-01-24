124c100767578d95e58530375d874e5b91f7e447
Armin Glavovic <glavovic.armin@gmail.com>
2022-01-24 22:57:48 +0000

    added headers

--- express_server.js
+++ express_server.js
@@ -14,7 +14,7 @@ app.get("/", (req, res) => {
 });
 
 app.get("/urls", (req, res) => {
-  const templateVars = { url : urlDatabase }
+  const templateVars = { urls : urlDatabase }
   res.render("urls_index", templateVars);
 });
 
