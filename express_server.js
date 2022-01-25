0260a978ed6b693a33c3a2730405db1c2d9d6a1a
Armin Glavovic <glavovic.armin@gmail.com>
2022-01-25 01:02:59 +0000

    added urls_new along with the page and created a generateRandomString function

--- express_server.js
+++ express_server.js
@@ -1,7 +1,20 @@
 const express = require("express");
 const app = express();
 const PORT = 8080;
+const bodyParser = require("body-parser");
 
+// generate unique string length of 6 characters
+const generateRandomString = () => {
+  let result = '';
+  let characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  for (let i = 0; i < 6; i++) {
+    result += characters.charAt(Math.floor(Math.random() * characters.length))
+  }
+  console.log(result)
+  return result
+};
+
+app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs")
 
 const urlDatabase = {
@@ -18,8 +31,17 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", templateVars);
 });
 
+app.post("/urls", (req, res) => {
+  console.log(req.body);
+  res.send("Ok")
+})
+
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase)
+});
+
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
 });
 
 app.get("/hello", (req, res) => {
@@ -30,8 +52,6 @@ app.get("/urls/:shortURL", (req, res) => {
   const templateVars = { shortURL: req.params.shortURL, longURL: req.params.longURL};
   res.render("urls_show", templateVars);
 });
-
-
 
 
 app.listen(PORT, () => {
