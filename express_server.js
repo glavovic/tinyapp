133c3c0f8cb9aca3f3aebd19e2fd338eb5ceb597
Armin Glavovic <glavovic.armin@gmail.com>
2022-01-27 06:50:42 +0000

    added unique userid interface, edit and delete funtion

--- express_server.js
+++ express_server.js
@@ -22,10 +22,37 @@ const verifyPassword = (password, key) => {
   return password === key.password;
 };
 
+const urlsForUser = (id, database) => {
+
+  const userURLs = {};
+
+  for (const shortURL in database) {
+    if(database[shortURL].userID === id) {
+    userURLs[shortURL] = database[shortURL];
+    }
+  }
+  return userURLs
+}
+// generate unique string length of 6 characters
+const generateRandomString = () => {
+  let result = '';
+  let characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  for (let i = 0; i < 6; i++) {
+    result += characters.charAt(Math.floor(Math.random() * characters.length));
+  }
+  return result;
+};
+
 
 const urlDatabase = {
-  "b2xVn2": "http://www.lighthouselabs.ca",
-  "9sm5xK": "http://www.google.com",
+  b6UTxQ: {
+      longURL: "https://www.tsn.ca",
+      userID: "aJ48lW"
+  },
+  i3BoGr: {
+      longURL: "https://www.google.ca",
+      userID: "aJ48lW"
+  }
 };
 
 const users = {
@@ -41,59 +68,64 @@ const users = {
   }
 };
 
-// generate unique string length of 6 characters
-const generateRandomString = () => {
-  let result = '';
-  let characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-  for (let i = 0; i < 6; i++) {
-    result += characters.charAt(Math.floor(Math.random() * characters.length));
-  }
-  return result;
-};
+
+
 
 
 app.get("/urls/new", (req, res) => {
   const user_id = req.cookies.user_id;
   const user = users[user_id];
+  if(!user) {
+    res.redirect("/login")
+  }
   const templateVars = { user, shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
   res.render("urls_new", templateVars);
-});
-
-app.get("/urls.json", (req, res) => {
-  const user_id = req.cookies.user_id;
-  const user = users[user_id];
-  const templateVars = { user, urls: urlDatabase };
-  res.render("urls_index", templateVars);
 });
 
 app.get("/urls/:shortURL", (req, res) => {
   const user_id = req.cookies.user_id;
   const user = users[user_id];
-  const templateVars = { user, urls: urlDatabase, shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
+  const templateVars = { user, urls: urlDatabase, shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL].longURL };
   res.render("urls_show", templateVars);
 });
 
 app.get("/urls", (req, res) => {
   const user_id = req.cookies.user_id;
   const user = users[user_id];
-  const templateVars = { user, urls: urlDatabase };
-  res.render("urls_index", templateVars);
+  if (user) {
+    const templateVars = { user, urls: urlsForUser(user, urlDatabase) };
+    res.render("urls_index", templateVars);
+  }
+  res.clearCookie("user_id")
+  res.redirect("/login");
 });
 
-app.get("/hello", (req, res) => {
-  res.send("<html><body>Hello <b>World</b></body></html>\n");
+app.get("/login", (req, res) => {
+  const user = req.cookies.user_id
+  if (user) return res.redirect("/urls")
+  const templateVars = {user : null};
+  res.render("urls_login", templateVars);
 });
+
 
 app.get("/u/:shortURL", (req, res) => {
   const shortURL = req.params.shortURL;
-  const longURL = urlDatabase[shortURL];
+  const longURL = urlDatabase[shortURL].longURL;
   res.redirect(longURL);
 });
 
 app.post("/u/:shortURL", (req, res) => {
-  const shortURL = req.params.shortURL;
-  urlDatabase[shortURL] = req.body.newURL;
-  res.redirect("/urls");
+  const user_id = req.cookies.user_id;
+  const user = users[user_id];
+    if(user) {
+      const shortURL = req.params.shortURL;
+      urlDatabase[shortURL] = {
+        longURL: req.body.newURL,
+        userID: user
+      };
+      res.redirect("/urls")
+    };
+  res.status(403).res.send("have to be loged in to preform that activity")
 });
 
 app.post("/logout", (req, res) => {
@@ -101,15 +133,16 @@ app.post("/logout", (req, res) => {
   res.redirect("/urls");
 });
 
-app.get("/login", (req, res) => {
-  const templateVars = {user : null};
-  res.render("urls_login", templateVars);
-});
 
 app.post("/urls/:shortURL/delete", (req, res) => {
-  const shortURL = req.params.shortURL;
-  delete urlDatabase[shortURL];
-  res.redirect("/urls");
+  const user_id = req.cookies.user_id;
+  const user = users[user_id];
+  if(user) {
+    const shortURL = req.params.shortURL;
+    delete urlDatabase[shortURL];
+    res.redirect("/urls");
+  }
+  res.status(403).send("have to be logged in for that activity")
 });
 
 app.post("/login", (req, res) => {
@@ -132,7 +165,17 @@ app.post("/login", (req, res) => {
 
 app.post("/urls", (req, res) => {
   let id = generateRandomString();
-  urlDatabase[id] = req.body.longURL;
+    const user_id = req.cookies.user_id;
+  const user = users[user_id];
+  if (!user) {
+    res.redirect("/login")
+    return res.statusCode(403).redirect("/login")
+  }
+  console.log(req.body)
+  urlDatabase[id] = {
+    longURL: req.body.longURL,
+    userID: user
+  }
   res.redirect('/urls/' + id);
 });
 
